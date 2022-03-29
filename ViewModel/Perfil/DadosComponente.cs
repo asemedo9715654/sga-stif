b4073f534b39b3b8094d6ce8c84691c1db75ae1d
Angelo <Semedo>
2022-03-29 15:53:52 -0100

    feito checkbox

---
+++ ViewModel/Perfil/DadosComponente.cs
@@ -0,0 +1,10 @@
+namespace sga_stif.ViewModel.Perfil
+{
+    public class DadosComponente
+    {
+        public int IdPerfil { get; set; }
+        public int IdMenuAcao { get; set; }
+        public bool Preenchido { get; set; } =false;
+        
+    }
+}
