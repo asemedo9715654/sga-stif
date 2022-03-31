a430e54d7634d0c063315871675d571b543b8856
Angelo <Semedo>
2022-03-31 17:24:24 -0100

    retificação das vistas

--- ViewModel/Beneficiario/NovoBeneficiarioViewModel.cs
+++ ViewModel/Beneficiario/NovoBeneficiarioViewModel.cs
@@ -9,12 +9,17 @@ namespace sga_stif.ViewModel.Beneficiario
         public int IdBeneficiario { get; set; }
         public string Nome { get; set; }
         public string Apelido { get; set; }
+        [Display(Name="Gráu de Parentesco")]
         public GrauDeParentesco GrauDeParentesco { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+          [Display(Name="Data de Nascimento")]
         public DateTime DataDeNascimento { get; set; }
+          [Display(Name="Número de Identificação Fiscal")]
         public string Nif { get; set; }
+          [Display(Name="Cni-BI")]
         public string CinBi { get; set; }
+          [Display(Name="Número de Passaporte")]
         public string NumeroPassaporte { get; set; }
         public int IdSocio { get; set; }
     }
