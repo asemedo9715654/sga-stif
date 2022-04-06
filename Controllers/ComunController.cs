ecc3f0a750acadee8ad67789c9b94ee15fa75e12
Angelo <Semedo>
2022-04-06 16:49:11 -0100

    tree view

---
+++ Controllers/ComunController.cs
@@ -0,0 +1,72 @@
+using AspNetCoreHero.ToastNotification.Abstractions;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+using sga_stif.Models;
+using sga_stif.ViewModel.Utilizador;
+
+namespace sga_stif.Controllers
+{
+    public class ComunController : Controller
+    {
+
+        private readonly ContextoBaseDados _context;
+
+        private readonly INotyfService _notyf;
+        private readonly IMapper _mapper;
+
+        private readonly ILogger<UtilizadorController> _logger;
+
+        public ComunController(ContextoBaseDados context, INotyfService notyf, IMapper mapper, ILogger<UtilizadorController> logger)
+        {
+            _context = context;
+            _notyf = notyf;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult GerarAcaoMaster()
+        {
+
+            var menus = _context.Menu.ToList();
+
+            foreach (var item in menus)
+            {
+                var a = new Acao()
+                {
+                    Nome = item.Nome,
+                    AcaoMaster = true
+                };
+
+                _context.Acao.Add(a);
+                _context.SaveChanges();
+
+
+                var menuAcao = new MenuAcao()
+                {
+                    IdAcao = a.IdAcao,
+                    IdMenu = item.IdMenu,
+                    MenuAcaoMaster = true,
+
+                };
+
+                _context.MenuAcao.Add(menuAcao);
+                _context.SaveChanges();
+
+            }
+
+
+
+            return Json(2);
+
+        }
+
+
+
+
+    }
+}
--- Models/EntidadeBase.cs
+++ Models/EntidadeBase.cs
@@ -4,12 +4,8 @@ namespace sga_stif.Models
 {
     public class EntidadeBase
     {
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime? DataCriacao { get; set; } = DateTime.Now;
-        
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+       
+        public DateTime? DataCriacao { get; set; } = DateTime.Now;      
         public DateTime? DataAtualizacao { get; set; } = DateTime.Now;
         public bool? Eliminado { get; set; } = false;
     }
